29120afef9a88cda6a3452333e4024c39a8ce1bb
LYZ <q751264263@vip.qq.com>
2018-04-21 23:12:00 +0800

    #add Google demo

---
+++ MyAndroidFrame/src/main/java/com/lvgodness/myandroid/activity/BasicActivity.java
@@ -0,0 +1,39 @@
+package com.lvgodness.myandroid.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import com.lvgodness.myandroid.R;
+
+public class BasicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(BasicActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+            }
+        });
+    }
+
+}
--- MyAndroidFrame/src/main/java/com/lvgodness/myandroid/activity/MainActivity.java
+++ MyAndroidFrame/src/main/java/com/lvgodness/myandroid/activity/MainActivity.java
@@ -1,17 +1,38 @@
 package com.lvgodness.myandroid.activity;
 
-import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.lvgodness.myandroid.R;
 import com.lvgodness.myandroid.base.BaseActivity;
 
 import butterknife.BindView;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
 
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
+//    @BindView(R.id.toolbar)
+//    Toolbar toolbar;
+    @BindView(R.id.button1)
+    Button button1;
+    @BindView(R.id.button2)
+    Button button2;
+    @BindView(R.id.button3)
+    Button button3;
+    @BindView(R.id.button4)
+    Button button4;
+    @BindView(R.id.button5)
+    Button button5;
+    @BindView(R.id.button6)
+    Button button6;
+    @BindView(R.id.button7)
+    Button button7;
+    @BindView(R.id.button8)
+    Button button8;
+    @BindView(R.id.button9)
+    Button button9;
+    @BindView(R.id.button10)
+    Button button10;
 
     @Override
     public int getLayoutId() {
@@ -20,8 +41,8 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initView() {
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -31,6 +52,52 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initListener() {
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        button10.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                startActivity(BasicActivity.class);
+                break;
+            case R.id.button2:
+                startActivity(BottomNavigationActivity.class);
+                break;
+            case R.id.button3:
+                startActivity(EmptyActivity.class);
+                break;
+            case R.id.button4:
+                startActivity(FullscreenActivity.class);
+                break;
+            case R.id.button5:
+                startActivity(LoginActivity.class);
+                break;
+            case R.id.button6:
+                startActivity(MasterDetailActivity.class);
+                break;
+            case R.id.button7:
+                startActivity(NavigationDrawerActivity.class);
+                break;
+            case R.id.button8:
+                startActivity(ScrollingActivity.class);
+                break;
+            case R.id.button9:
+                startActivity(SettingsActivity.class);
+                break;
+            case R.id.button10:
+                startActivity(TabbedActivity.class);
+                break;
+
+        }
     }
 }
